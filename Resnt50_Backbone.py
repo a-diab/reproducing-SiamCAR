17d6f943ee2317cede7e9bf2ea8afe32dd31a796
bijou-bijou <39841944+bijou-bijou@users.noreply.github.com>
2021-04-15 22:40:22 -0400

    Update Resnt50_Backbone.py

--- Resnt50_Backbone.py
+++ Resnt50_Backbone.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
 
 
 import torch 
@@ -10,10 +9,6 @@ import numpy as np
 import torchvision.transforms as transforms 
 import torchvision 
 import torch.nn.functional as F
-
-
-# In[ ]:
-
 
 class Basic_Block(nn.Module):
     expansion = 1
@@ -40,10 +35,6 @@ class Basic_Block(nn.Module):
         output = F.relu(output)
 
         return output
-
-
-# In[ ]:
-
 
 class bottleNeck(nn.Module):
     expansion = 4    # to expand the number of channels by 4 
@@ -72,10 +63,6 @@ class bottleNeck(nn.Module):
         output = F.relu(output)
         return output
 
-
-# In[ ]:
-
-
 class ResNet(nn.Module):
     def __init__(self,block,num_layers,used_layers):
         super(ResNet,self).__init__()
@@ -83,15 +70,40 @@ class ResNet(nn.Module):
         self.input_planes = 64
         self.conv1 = nn.Conv2d(3,64,kernel_size=7,stride=2,padding=0, bias=False)
         self.bn1   = torch.nn.BatchNorm2d(64)
-        
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)  # diff
-         
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)  
         self.layer1 = self._layer(block,64,num_layers[0],stride=1)
         self.layer2 = self._layer(block,128,num_layers[1],stride=2)
-        self.layer3 = self._layer(block,256,num_layers[2],stride=2)
-        self.layer4 = self._layer(block,512,num_layers[3],stride=2)
+        
+        
         self.averagePool = torch.nn.AvgPool2d(kernel_size=4,stride=1)
-        self.fc    =  torch.nn.Linear(512*block.expansion,used_layers)
+        self.fc    =  torch.nn.Linear(512*block.expansion,used_layers)##########
+        
+        self.feature_size = 128 * block.expansion
+        self.used_layers = used_layers
+        layer3 = True if 3 in used_layers else False
+        layer4 = True if 4 in used_layers else False
+
+        if layer3:
+            self.layer3 = self._layer(block,256,num_layers[2],stride=1, dilation=2)      # different from The original ResNet(stride=2) 15x15, 7x7
+            self.feature_size = (256 + 128) * block.expansion
+        else:
+            self.layer3 = lambda x: x  # identity
+
+        if layer4:
+            self.layer4 = self._layer(block,512,num_layers[3],stride=1, dilation=4)       # The original ResNet(stride=2) 7x7, 3x3
+            self.feature_size = 512 * block.expansion
+        else:
+            self.layer4 = lambda x: x  # identity
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+                
+# In[ ]:
 
 def _layer(self,block,output_planes,num_layers,stride=1):
         dim_change = None
@@ -109,8 +121,7 @@ def _layer(self,block,output_planes,num_layers,stride=1):
     
     def forward(self,x):
         x1 = F.relu(self.bn1(self.conv1(x)))
-        x = self.maxpool(x1)                 # diff
-
+        x = self.maxpool(x1)               
         p1 = self.layer1(x)
         p2= self.layer2(p1)
         p3= self.layer3(p2)
@@ -133,20 +144,6 @@ def resnet50(nn.model):
     model = ResNet(Bottleneck, [3, 4, 6, 3])
     return model
 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
